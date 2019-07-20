098456bb4af6b32078f62bde77c9aa665de93b01
AbseliamovEnver <a.enver.com@gmail.com>
2019-07-20 13:02:30 +0300

    Moving method getDevelopers to JavaIODeveloperRepositoryImpl.

--- src/main/java/com/abseliamov/javacoreio/repository/JavaIODeveloperRepositoryImpl.java
+++ src/main/java/com/abseliamov/javacoreio/repository/JavaIODeveloperRepositoryImpl.java
@@ -1,14 +1,18 @@
 package com.abseliamov.javacoreio.repository;
 
+import com.abseliamov.javacoreio.controller.SkillController;
+import com.abseliamov.javacoreio.model.Account;
+import com.abseliamov.javacoreio.model.AccountStatus;
 import com.abseliamov.javacoreio.model.Developer;
 import com.abseliamov.javacoreio.model.Skill;
-import com.abseliamov.javacoreio.utils.DeveloperUtil;
 import com.abseliamov.javacoreio.utils.IOUtil;
 
 import java.io.*;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
     public static final String DEVELOPER_FILE = "src/main/resources/developers.txt";
@@ -53,7 +57,7 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
         Set<Developer> developers;
 
         if (file.length() != 0) {
-            developers = DeveloperUtil.getDevelopers(file);
+            developers = getDevelopers(file);
             for (Developer developer : developers) {
                 if (developer.getId() == id) {
                     return developer;
@@ -70,7 +74,7 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
         Set<Developer> developers;
 
         if (file.length() != 0) {
-            developers = DeveloperUtil.getDevelopers(file);
+            developers = getDevelopers(file);
             for (Developer developer : developers) {
                 if (developer.getFirstName().equals(name))
                     return developer;
@@ -85,7 +89,7 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
         Set<Developer> developers = null;
 
         if (file.length() != 0) {
-            developers = DeveloperUtil.getDevelopers(file);
+            developers = getDevelopers(file);
         } else System.out.println("File with developers is empty");
         return developers;
     }
@@ -102,7 +106,7 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
         }
 
         if (file.length() != 0) {
-            Set<Developer> developers = DeveloperUtil.getDevelopers(file);
+            Set<Developer> developers = getDevelopers(file);
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(DEVELOPER_FILE, false))) {
                 for (Developer developerItem : developers) {
                     if (developerItem.getId() == id) {
@@ -133,7 +137,7 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
         Set<Long> skillsId = new HashSet<>();
 
         if (file.length() != 0) {
-            Set<Developer> developerList = DeveloperUtil.getDevelopers(file);
+            Set<Developer> developerList = getDevelopers(file);
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(DEVELOPER_FILE, false))) {
                 for (Developer developer : developerList) {
                     if (developer.getId() == id) {
@@ -169,7 +173,7 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
             skillsIdNew.add(skill.getId());
 
         if (file.length() != 0) {
-            Set<Developer> developerList = DeveloperUtil.getDevelopers(file);
+            Set<Developer> developerList = getDevelopers(file);
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(DEVELOPER_FILE, false))) {
                 for (Developer developerItem : developerList) {
                     if (developerItem.getId() == developer.getId()) {
@@ -191,5 +195,49 @@ public class JavaIODeveloperRepositoryImpl implements DeveloperRepository {
             }
         }
         return addSkill;
+    }
+
+    private Set<Developer> getDevelopers(File file) {
+        Set<Developer> developers = new HashSet<>();
+        SkillController skillController = new SkillController();
+        Set<Skill> skillSet;
+        skillSet = skillController.getListSkills();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+            String data;
+            while ((data = reader.readLine()) != null) {
+                String[] item = data.split(",");
+                long idDeveloper = Long.parseLong(item[0]);
+                String firstName = item[1];
+                String lastName = item[2];
+                long idAccount = Long.parseLong(item[item.length - 2]);
+                AccountStatus status = AccountStatus.valueOf(item[item.length - 1]);
+
+                String regex = "\\[.*\\]";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(data);
+
+                String group = null;
+                if (matcher.find()) {
+                    group = matcher.group();
+                }
+                String[] skillId = group.substring(1, group.length() - 1).split(",");
+
+                Set<Skill> skills = new HashSet<>();
+                for (int i = 0; i < skillId.length; i++) {
+                    long id = Long.parseLong(skillId[i].trim());
+                    for (Skill skillItem : skillSet) {
+                        if (skillItem.getId() == id)
+                            skills.add(new Skill(id, skillItem.getSkillName()));
+                    }
+                }
+                developers.add(new Developer(idDeveloper, firstName, lastName, skills, new Account(idAccount, status)));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Exception reading file in method get developer by id: " + e);
+        }
+        return developers;
     }
 }
